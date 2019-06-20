1dd838786eaaa724eb722c44565f533bb596da07
Stefan Ring <stefanrin@gmail.com>
2019-06-20 22:28:02 +0200

    alku: Same thing for hoi

---
+++ ALKU/CUT64KB.C
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+static void shipline(FILE *fout, char *line, int n)
+{
+    int c, n1 = n-1;
+    fprintf(fout, "\tdb\t");
+    for (c=0; c < n1; c++)
+        fprintf(fout, "%d,", (unsigned char) line[c]);
+    fprintf(fout, "%d\n", (unsigned char) line[c]);
+}
+
+int main(int argc, char *argv[])
+{
+    char namebuf[128];
+    char line[16];
+    int n, ch, linepos, seeneof=0;
+    long count;
+    FILE *fout, *fin;
+
+    if (argc < 3)
+        return 1;
+    fin = fopen(argv[1], "rb");
+    if (!fin)
+        return 1;
+    for (n=0; !seeneof; n++) {
+        sprintf(namebuf, "%s%d", argv[2], n);
+        fout = fopen(namebuf, "w");
+        linepos = 0;
+        for (count=0; count<0xffffL; count++)
+        {
+            ch = getc(fin);
+            if (ch == EOF)
+            {
+                seeneof = 1;
+                break;
+            }
+            line[linepos++] = ch;
+            if (linepos == 16)
+            {
+                shipline(fout, line, 16);
+                linepos = 0;
+            }
+        }
+        if (linepos)
+            shipline(fout, line, linepos);
+        fclose(fout);
+    }
+    return 0;
+}
--- ALKU/TAIL.C
+++ ALKU/TAIL.C
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     char buf[256];
-    int cnt=0;
+    int cnt=0, lines;
+    char *parse;
+    if (argc > 1)
+        lines = strtol(argv[1], &parse, 10);
+    else
+        lines = 50;
     while (fgets(buf, 256, stdin)) {
-        if (cnt++ < 50)
+        if (cnt++ < lines)
             continue;
         printf("%s", buf);
     }
